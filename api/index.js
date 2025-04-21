691011c1129da4af37e255ee65865cc60ec8135d
mashrurhossainkhan <khanmashrur03@gmail.com>
2025-04-21 21:13:33 +0600

    adding a route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello from Express on Vercel!');
 });
 
+app.post('/echo', (req, res) => {
+    const data = req.body;
+    console.log('Received POST data:', data);
+    res.json({ received: data });
+  });
+  
 module.exports = app;
